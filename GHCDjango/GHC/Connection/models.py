dedce74cb62f822b7d2bd3861b62bed3c97df433
zackakyo <tcheuyassiisaac@yahoo.com>
2021-11-01 12:51:36 +0100

    update

--- GHCDjango/GHC/Connection/models.py
+++ GHCDjango/GHC/Connection/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.db.models import deletion
+from django.db.models.base import Model
+from django.db.models.fields import DateField, DateTimeField
+from django.db.models.fields.related import ForeignKey
+from django.db.models.manager import ManagerDescriptor
 
 # Create your models here.
+
+class Personne(models.Model): 
+    nom = models.CharField(max_length=255, null = True)
+    prenom = models.CharField(max_length=255, null = True)
+    fonction = models.CharField(max_length=255, null = True)
+    statut = models.CharField(max_length=255, null = True)
+    email = models.EmailField(max_length=255, null = True)
+    mdp = models.CharField(max_length=255, null = True)
+    photo = models.ImageField( null = True)
+    # photo = models.URLField()
+    dateEnregistrement = models.DateTimeField( auto_now_add=True)
+
+class Formation(models.Model):
+    nom = models.CharField(max_length=255, null=False, unique=True )
+    lieu = models.CharField(max_length=255)
+    responsable = models.ForeignKey(Personne, on_delete=models.CASCADE)
+    personnes = models.ManyToManyField(Personne, related_name='formations', blank=True)
+
+class Professeur(Personne):
+    pass
+    # professeur varchar(255) not null,
+    # matiere varchar(255) not null
+
+class Matiere(models.Model):
+    nom = models.CharField(max_length=255, null=False)
+    totalDuration = models.DurationField()
+    currentDuration = models.DurationField()
+    dailyDuration = models.DurationField()
+    professeur = models.ManyToManyField(Professeur, related_name='matieres')
+
+
+class Horaire(models.Model):
+    joursDeCours = models.DateField()
+    heureDeDebut = models.TimeField()
+    specificite = models.CharField(max_length=255)
+    aEuLieu = models.BooleanField(default=False)
+    remarque = models.CharField(max_length=255) 
+    matiere = models.ForeignKey(Matiere, on_delete=models.CASCADE)
+    formations = models.ManyToManyField(Formation, related_name='horaires', blank=True)
