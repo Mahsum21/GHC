0f20dbd9874a7b329898bdf9cf4c7523cbc52260
zackakyo <tcheuyassiisaac@yahoo.com>
2021-12-16 12:56:43 +0100

    update

--- GHCDjango/GHC/GHC/settings.py
+++ GHCDjango/GHC/GHC/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'GHC.urls'
